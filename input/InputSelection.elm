4b7f16c7c00c258b6f792b483ccd535be6659649
trotha01 <trevor.rothaus@sendgrid.com>
2017-04-28 16:30:43 -0700

    added input selection example

---
+++ input/InputSelection.elm
@@ -0,0 +1,78 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, input, div)
+import Html.Attributes exposing (value)
+import Html.Events exposing (on)
+import Json.Decode as Json
+
+
+main =
+    Html.beginnerProgram { model = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { selection : Selection
+    }
+
+
+type alias Selection =
+    { start : Int
+    , end : Int
+    }
+
+
+initSelection : Selection
+initSelection =
+    { start = 0
+    , end = 0
+    }
+
+
+init : Model
+init =
+    { selection = initSelection }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Select Selection
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Select selection ->
+            { model | selection = selection }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ value "Hello, World!", on "select" selectionDecoder ]
+            []
+        , div
+            []
+            [ text ("Start: " ++ (toString model.selection.start)) ]
+        , div
+            []
+            [ text ("End: " ++ (toString model.selection.end)) ]
+        ]
+
+
+selectionDecoder : Json.Decoder Msg
+selectionDecoder =
+    Json.map Select <|
+        Json.map2 Selection
+            (Json.at [ "target", "selectionStart" ] Json.int)
+            (Json.at [ "target", "selectionEnd" ] Json.int)
